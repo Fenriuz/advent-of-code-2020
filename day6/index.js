2d8016c733d96a1e2edd7c63a88196a7a6b348a3
Alex <alxvalle99@gmail.com>
2021-01-08 22:40:49 -0600

    Day 6 added

---
+++ day6/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const answersData = fs.readFileSync('day6/answers.txt', { encoding: 'utf-8' });
+
+const partOne = (answers) => {
+  const groupAnswers = answers.split(/\r\n\s*\n/);
+}
